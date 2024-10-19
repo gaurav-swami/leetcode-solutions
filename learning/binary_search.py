8331a8961151ec29cdcecc85a9522b85cfcd2f00
Gaurav-Swami <gauravswami0404@gmail.com>
2024-10-20 01:39:11 +0530

    implemented recursive binary search

--- learning/binary_search.py
+++ learning/binary_search.py
@@ -5,20 +5,32 @@ def binary_search(arr: List[int], target: int)-> int:
 	end = len(arr)-1
 
 	while start<=end:
-		mid = (start+end)//2
+		mid = start + (end-start)//2
 
-		if arr[mid] == target:
-			return mid
-
-		elif arr[mid]> target:
+		if arr[mid] > target:
 			end = mid-1
-		elif arr[mid]<target:
+		elif arr[mid] < target:
 			start = mid+1
-
+		else:
+			return mid
 
 	return -1
 
-arr = [0,3,4,5,9,11,10,]
-target = 12
+def binary_search_recursive (arr: List[int],target: int, start: int, end: int  )-> int:
+	if  start<=end:
+		mid = start + (end-start)//2
+		if arr[mid]> target:
+			return binary_search_recursive(arr, target, start, mid-1)
+		elif arr[mid] < target:
+			return binary_search_recursive(arr,target,mid+1, end)
+		else:
+			return mid
+	else:
+		return -1
+
+arr = [0,3,4,5,9,11,12,]
+# arr = []
+target = 9
 
 print(binary_search(arr,target)) 
+print(binary_search_recursive(arr,target,0,len(arr)-1))
