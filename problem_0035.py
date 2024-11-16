72271788803e0bbc05c39dba0184e4e631d31917
Gaurav-Swami <gauravswami0404@gmail.com>
2024-11-17 00:28:12 +0530

    optimised problem 35

--- problem_0035.py
+++ problem_0035.py
@@ -1,30 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        def binarySearch (arr: List[int],target: int, start:int, end:int)->int:
-            mid = start + (end-start)//2
-            if start<=end:
-                if arr[mid] > target:
-                    if mid-1 == -1:
-                        return mid
-                    elif arr[mid-1]<target:
-                        return mid
-                    else:
-                        return binarySearch(arr,target,start,mid-1)
-                
-                elif arr[mid]<target:
-                    if mid+1 >= len(arr):
-                        return mid+1
-                    elif arr[mid+1] >target:
-                        return mid+1
-                    else:
-                        return binarySearch(arr,target,mid+1,end)
-                
-                else:
-                    return mid
-            return -1
+        left, right = 0, len(nums)-1
 
+        while left<=right:
+            mid = left + (right-left)//2
 
-
-            
-        
-        return binarySearch(nums,target,0,len(nums)-1)
+            if target==nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                right = mid-1
+            else: 
+                left = mid + 1        
+        return left
