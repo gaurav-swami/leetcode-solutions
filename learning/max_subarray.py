bef64d66e733d762b080c0d4c4e57d47565e8d6a
Gaurav-Swami <gauravswami0404@gmail.com>
2024-11-25 23:11:06 +0530

    solved problem-53 by kadane's algo

--- learning/max_subarray.py
+++ learning/max_subarray.py
@@ -13,7 +13,10 @@ def print_array(arr: List[int]):
 		print()
 
 
-def max_sum_bf(arr: List[int])->int:
+
+# least optimized with TM : n^3
+
+def max_sum_bf_1(arr: List[int]):
 	add	= 0
 	sums = []
 	for st in range(0,len(arr)):
@@ -23,6 +26,36 @@ def max_sum_bf(arr: List[int])->int:
 			sums.append([add,st,end])
 			add = 0
 
-	return max(sums)
+	print( max(sums))
 
-print(max_sum_bf(arr))
+# slightly optimized bf with TM: n^2
+
+def max_sum_bf_2 (arr:List[int]):
+	add = 0
+	sums = []
+	maxi = 0
+	for st in range(0,len(arr)):
+		add=0	
+		for end in range(st,len(arr)):
+			add+= arr[end]
+			maxi = max(maxi,add)
+		
+	print(maxi)
+
+# kadane's algo with TM: n
+
+
+def kadane(arr: List[int]):
+
+	lar_sum=0
+	curr_sum=0
+
+	for i in range(0,len(arr)):
+		curr_sum += arr[i]
+		lar_sum = max(curr_sum,lar_sum)
+		if curr_sum<0:
+			curr_sum=0
+
+	print(lar_sum)
+
+kadane(arr)
