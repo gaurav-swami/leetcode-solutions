0f96bae48290e82728fa5f5a12820b47dc0bbf9a
Gaurav-Swami <gauravswami0404@gmail.com>
2025-03-02 23:05:19 +0530

    solved problem 125 again

--- problem_0125.py
+++ problem_0125.py
@@ -3,18 +3,13 @@ class Solution:
         left = 0
         right = len(s)-1
 
-        while left <= right:
-            if not s[left].isalnum():
-                left += 1
-                continue
-            if not s[right].isalnum():
-                right -= 1
-                continue
-            
+        while left<=right:  
+            while left < right and not s[left].isalnum():
+                left+=1
+            while right > left and not s[right].isalnum():
+                right-=1
             if s[left].lower() != s[right].lower():
                 return False
-
-            left += 1
-            right -= 1
-        
+            left+=1
+            right-=1
         return True
