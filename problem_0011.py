d2038383f31df724ea025b07dea257e176f45fda
Gaurav-Swami <gauravswami0404@gmail.com>
2025-03-12 15:00:08 +0530

    did problem 11

---
+++ problem_0011.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]):
+        l,r = 0,len(height)-1
+        max_water = 0
+        while l<r:
+            if height[l]<height[r]:
+                max_water = max(max_water,(r-l)*height[l])
+                l+=1
+            else:
+                max_water = max(max_water,(r-l)*height[r])
+                r-=1
+        return max_water
