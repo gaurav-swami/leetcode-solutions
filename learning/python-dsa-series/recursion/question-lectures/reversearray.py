b97f92c740b5765f4ae04fd2d63e473a72d9dc95
Gaurav Swami <gauravswami0404@gmail.com>
2026-08-25 19:20:39 +0530

    new recursion questions

---
+++ learning/python-dsa-series/recursion/question-lectures/reversearray.py
@@ -0,0 +1,14 @@
+def reverse ( a, l=0, r=None):
+    if not r :
+        r = len (a)-1
+    if l>=r:
+        return 
+     
+    a[r], a[l] = a[l], a[r]
+    reverse( a, l+1, r-1)
+
+arr = [ 1, 2, 3, 4 ]
+
+reverse (arr)
+
+print (arr)
