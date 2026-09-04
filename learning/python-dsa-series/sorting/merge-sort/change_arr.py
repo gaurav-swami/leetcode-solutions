cf6343a0f9f523f3d1f0028ee44e40b1bb908e7c
Gaurav Swami <gauravswami0404@gmail.com>
2026-09-04 23:38:01 +0530

    recursion changing array at place

---
+++ learning/python-dsa-series/sorting/merge-sort/change_arr.py
@@ -0,0 +1,33 @@
+def merge_sort(arr,l,r):
+    if l>=r:
+        return 
+    mid = (l+r)//2
+    merge_sort(arr,l,mid)
+    merge_sort(arr,mid+1,r)
+    merge(arr,l,mid,r)
+
+def merge(arr,l,mid,r):
+    result = []
+    i,j = l,mid+1
+
+    while i<=mid and j<=r:
+        if arr[i]<=arr[j]:
+            result.append(arr[i])
+            i+=1
+        else:
+            result.append(arr[j])
+            j+=1
+    
+    while i<=mid:
+        result.append(arr[i])
+        i+=1
+    while j<=r:
+        result.append(arr[j])
+        j+=1
+
+    for i in range(len(result)):
+        arr[l+i] = result[i]
+
+arr = [2,3,53,3,7,86,5,54,75,34,65,54,63,22,1]
+merge_sort(arr,0,len(arr)-1) 
+print(arr) 
