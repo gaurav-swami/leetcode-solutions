d248051451f37f4ea289dd5e5cd0f47b4799d0c6
Gaurav Swami <gauravswami0404@gmail.com>
2026-09-04 12:35:00 +0530

    studied and implemented merge sort

---
+++ learning/python-dsa-series/sorting/merge-sort/program.py
@@ -0,0 +1,31 @@
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid = len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left,right)
+    
+def merge(left,right):
+    i,j = 0,0
+    n,m = len(left), len(right)
+    result = []
+
+    while i<n and j <m:
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i<n:
+        result.append(left[i])
+        i+=1
+    while j<m:
+        result.append(right[j])
+        j+=1
+    return result
+
+arr = [3,5,4,6,6,8,76,9,97,5,4,6,8,9]
+print(merge_sort(arr)) 
