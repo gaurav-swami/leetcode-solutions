ea803aba28602b17d613c6044d0c38efe98526f1
Gaurav Swami <gauravswami0404@gmail.com>
2026-09-04 11:04:38 +0530

    understood and implemented insertion-sort

---
+++ learning/python-dsa-series/sorting/insertion-sort/program_1.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+
+        while j>=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j-=1;
+
+        arr[j+1] = key
+
+arr = [3,5,6,4,8,9,10,7,1]   
+insertion_sort(arr)
+print(arr)
