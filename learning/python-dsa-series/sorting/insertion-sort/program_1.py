0eae1c065c33894012c6a39dec7c5c8c89027465
Gaurav Swami <gauravswami0404@gmail.com>
2026-09-05 23:55:28 +0530

    merge sort complete

--- learning/python-dsa-series/sorting/insertion-sort/program_1.py
+++ learning/python-dsa-series/sorting/insertion-sort/program_1.py
@@ -6,7 +6,7 @@ def insertion_sort(arr):
 
         while j>=0 and arr[j] > key:
             arr[j+1] = arr[j]
-            j-=1;
+            j-=1 
 
         arr[j+1] = key
 
--- learning/python-dsa-series/sorting/merge-sort/change_arr.py
+++ learning/python-dsa-series/sorting/merge-sort/change_arr.py
@@ -1,14 +1,14 @@
 def merge_sort(arr,l,r):
     if l>=r:
         return 
-    mid = (l+r)//2
+    mid = (l+r)//2   #r is the last index not the length so mid+1
     merge_sort(arr,l,mid)
     merge_sort(arr,mid+1,r)
     merge(arr,l,mid,r)
 
 def merge(arr,l,mid,r):
     result = []
-    i,j = l,mid+1
+    i,j = l,mid+1 #since r is not the length and l = 0 so m+1 
 
     while i<=mid and j<=r:
         if arr[i]<=arr[j]:
@@ -26,7 +26,7 @@ def merge(arr,l,mid,r):
         j+=1
 
     for i in range(len(result)):
-        arr[l+i] = result[i]
+        arr[l+i] = result[i]   #because we are changing the original array and l might not be 0
 
 arr = [2,3,53,3,7,86,5,54,75,34,65,54,63,22,1]
 merge_sort(arr,0,len(arr)-1) 
